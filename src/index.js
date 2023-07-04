47e782f2194024229607baf739d82b512c371879
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-07-04 20:18:12 +0600

    Chapter 05: Working With Components, Props, and JSX>> Lecture 16: Conditional Rendering With &&

--- src/index.js
+++ src/index.js
@@ -67,14 +67,21 @@ function Header() {
 }
 
 function Menu() {
+  // const pizzas = pizzaData;
+  const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
+
       {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, mushrooms, and onion"
@@ -113,7 +120,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleDateString()}, We are currently Open
+      {isOpen && (
+        <div className="order">
+          <p>
+            {" "}
+            We're open until {closeHour}:00. Come visit us or order online.{" "}
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 }
