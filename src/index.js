958232e00180ffad19115e418e032fba2a7425cc
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-07-04 21:40:07 +0600

    Chapter 05: Working With Components, Props, and JSX>> Lecture 17: Conditional Rendering With Ternaries

--- src/index.js
+++ src/index.js
@@ -67,21 +67,30 @@ function Header() {
 }
 
 function Menu() {
-  // const pizzas = pizzaData;
-  const pizzas = [];
+  const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {/* and operator for conditional rendering */}
+      {/* {numPizzas > 0 && (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      )} */}
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back leter :)</p>
       )}
-
       {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, mushrooms, and onion"
@@ -120,7 +129,7 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {/* {isOpen && (
         <div className="order">
           <p>
             {" "}
@@ -128,6 +137,20 @@ function Footer() {
           </p>
           <button className="btn">Order</button>
         </div>
+      )} */}
+      {/* and operator for conditional rendering */}
+      {isOpen ? (
+        <div className="order">
+          <p>
+            {" "}
+            We're open until {closeHour}:00. Come visit us or order online.{" "}
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00 .
+        </p>
       )}
     </footer>
   );
